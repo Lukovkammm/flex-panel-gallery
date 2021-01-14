ea4cd60b49dd1d20c4faccf3923d9fb310d61ee1
Marina <artyusmarina@yandex.ru>
2021-01-14 13:08:39 +0300

    fix: fix script for active text

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function switcherPanel(e) {
     if (e.target.classList.contains('panel')) {
         panels.forEach(panel => panel.classList.remove('panel_open'));
         e.target.classList.toggle('panel_open');
+        activeText();
     }
 }
 
-function activeText() {
-    this.classList.toggle('panel_active');
+function activeText(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('panel_active');
+    }
 }
 
 parentPanel.addEventListener('click', switcherPanel);
