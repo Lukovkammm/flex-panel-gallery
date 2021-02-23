9ad0c5b319a85b2e589cf6077e4dff093a3707c5
Marina <artyusmarina@yandex.ru>
2021-02-23 16:09:52 +0300

    refactor: change structure

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const panels = document.querySelectorAll('.panel');
 const parentPanel = document.querySelector('.panels');
- 
-function switcherPanel(e) {
-    if (e.target.classList.contains('panel')) {
-        panels.forEach(panel => panel.classList.remove('panel_open'));
-        e.target.classList.toggle('panel_open');
-        activeText();
-    }
-}
 
-function activeText(e) {
-    if(e.propertyName.includes('flex')) {
-        this.classList.toggle('panel_active');
+function switcherPanel(e) {
+    panels.forEach(panel => panel.classList.remove('panel_open'));
+    panels.forEach(panel => panel.classList.remove('panel_active'));
+    if (e.target.classList.contains('panel')) {
+        e.target.classList.add('panel_open');
+        e.target.classList.add('panel_active');
     }
 }
 
 parentPanel.addEventListener('click', switcherPanel);
-panels.forEach(panel => panel.addEventListener('transitionend', activeText));
